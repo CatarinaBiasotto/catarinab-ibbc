899c00d85369bec67120ef6a1eb920c31f313b76
CatarinaBiasotto <catarina.biasotto@hotmail.com>
2024-12-03 20:09:34 +0000

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -35,7 +35,7 @@ do
   file2=$(echo "$file2" | tr -d '\r')
 
   # Executa o trimmomatic para as amostras em par (PE) rawdata, guarda output (trimp e trimu) e ficheiro log com resumi de informações na pasta finaldata
-  trimmomatic PE -phred33 "$file1" "$file2" "/home/catarina/ibbc/teste/finaldata/${sample}_file1.trimP.fastq.gz" "/home/catarina/ibbc/teste/finaldata/${sample}_file1.trimU.fastq.gz" "/home/catarina/ibbc/teste/finaldata/${sample}_file2.trimP.fastq.gz" "/home/catarina/ibbc/teste/finaldata/${sample}_file2.trimU.fastq.gz" SLIDINGWINDOW:4:20 >> "/home/catarina/ibbc/teste/finaldata/logs/${sample}_trimmomatic.log" 2>&1
+  trimmomatic PE -phred33 "$file1" "$file2" "/home/fc54205/teste/finaldata/${sample}_file1.trimP.fastq.gz" "/home/fc54205/teste/finaldata/${sample}_file1.trimU.fastq.gz" "/home/fc54205/teste/finaldata/${sample}_file2.trimP.fastq.gz" "/home/fc54205/teste/finaldata/${sample}_file2.trimU.fastq.gz" SLIDINGWINDOW:4:20 >> "/home/fc54205/teste/finaldata/logs/${sample}_trimmomatic.log" 2>&1
   echo "---!TRIMOMMATIC finalizado para $sample com arquivos: $file1 e $file2!---"
 
 done < "$txt_file"
@@ -55,8 +55,8 @@ do
   if [[ "$sample" == "$sample" ]]; then
 
   # Executa o FastQC para o par das amostras em finaldata
-  fastqc "/home/catarina/ibbc/teste/finaldata/${sample}_file1.trimP.fastq.gz" "/home/catarina/ibbc/teste/finaldata/${sample}_file2.trimP.fastq.gz" -o /home/catarina/ibbc/teste/finaldata/
-  echo "---!FASTQC finalizado para $sample com arquivos: "/home/catarina/ibbc/teste/finaldata/${sample}_file1.trimP.fastq.gz" e "/home/catarina/ibbc/teste/finaldata/${sample}_file2.trimP.fastq.gz"---"
+  fastqc "/home/fc54205/teste/finaldata/${sample}_file1.trimP.fastq.gz" "/home/fc54205/teste/finaldata/${sample}_file2.trimP.fastq.gz" -o /home/fc54205/teste/finaldata/
+  echo "---!FASTQC finalizado para $sample com arquivos: "/home/fc54205/teste/finaldata/${sample}_file1.trimP.fastq.gz" e "/home/fc54205/teste/finaldata/${sample}_file2.trimP.fastq.gz"---"
   
   fi
 done < "$txt_file"
@@ -65,7 +65,7 @@ done < "$txt_file"
 echo "Processamento FASTQC para as amostras finais completo!" 
 
   # Executa o Multiqc para todas as amostras processadas para ver as amostras com os pares juntos
-  multiqc /home/catarina/ibbc/teste/finaldata/*.zip* -o /home/catarina/ibbc/teste/finaldata/
+  multiqc /home/fc54205/teste/finaldata/*.zip* -o /home/fc54205/teste/finaldata/
 
 # Informa que terminou o processamento multiqc
 echo "Processamento MULTIQC para as amostras finais completo!"
